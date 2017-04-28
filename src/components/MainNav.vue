049349153c5ac4e897a6b717c53eccb43c2e997e
Kevin Restaino <kevinrestaino@gmail.com>
2017-04-28 15:42:31 -0500

    SCSS setup

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="mainNav">
-    <a href="/">Portfolio</a>
-    <a href="resume">Resume</a>
+    <router-link tag="a" to="/">Portfolio</router-link>
+    <router-link tag="a" to="/resume">Resume</router-link>
+    <span>|</span>
+    <a href="https://github.com/krestaino">GitHub</a>
+    <a href="https://www.linkedin.com/in/kevinrestaino/">LinkedIn</a>
   </div>
 </template>
 
@@ -11,8 +14,22 @@ export default {
 }
 </script>
 
-<style scoped>
-a {
-  font-weight: bold;
+<style scoped lang="scss">
+.mainNav {
+  align-items: center;
+  display: flex;
+  justify-content: center;
+
+  a, span {
+    margin-left: 16px;
+
+    &:first-child {
+      margin-left: 0;
+    }
+
+    &.router-link-exact-active {
+      font-weight: bold;
+    }
+  }
 }
 </style>
