c3c033a5fdf54164f9a91ee85e37a7876275db67
jasonc1025 <jasonc@eande.world>
2018-04-10 17:21:18 -0700

    Label classes as '...Style'

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="workStyle">
-    <ul class="projectsStyle">
+  <div class="divStyle">
+    <ul class="ulStyle">
       <li v-for="project in projects">
 
         <a class="button" target="_blank" :href="project.url">
         <!-- <router-link tag="a" :to="`/work/${project.category}/${project.slug}`"> -->
         
           <!-- [jwc] Need to embed both 'title' fields in the same '<h3>' with a '<b>' inbetween -->
-          <h3>Press to Enter {{ project.title }}... <br> {{ project.title02 }}</h3>
+          <h3>Press to Enter Live Site: {{ project.title }}... <br> {{ project.title02 }}</h3>
           <!-- [jwc] not work, will overlay prior <h3>{{ project.title02 }}</h3> -->
           <!-- <h3> Hello2 {{ project.title }} </h3> -->
           <h2> {{ project.headerLine01 }} </h2>
@@ -51,7 +51,7 @@ h2 {
 
 }
 
-.projectsStyle {
+.ulStyle {
   display: flex;
   flex-wrap: wrap;
 
