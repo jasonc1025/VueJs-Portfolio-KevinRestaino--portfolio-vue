bf9d4dfbd89eaf29f39c9a8c90b6a60ebe914d39
jasonc1025 <jasonc@eande.world>
2018-04-12 15:59:09 -0700

    Improve Overlay Text

--- src/components/WorkVueJs.vue
+++ src/components/WorkVueJs.vue
@@ -8,7 +8,7 @@
         <!-- <router-link tag="a" :to="`/work/${project.category}/${project.slug}`"> -->
         
           <!-- [jwc] Need to embed both 'title' fields in the same '<h3>' with a '<b>' inbetween -->
-          <h3>Press to Enter Live Demo Site: <br> {{ project.title }} <br> {{ project.title02 }}</h3>
+          <h3>Watch this current 60sec Screencast <br> - or - <br> Press to Enter Live Demo Site: <br> {{ project.title }} <br> {{ project.title02 }}</h3>
           <!-- [jwc] not work, will overlay prior <h3>{{ project.title02 }}</h3> -->
           <!-- <h3> Hello2 {{ project.title }} </h3> -->
           <h1> {{ project.headerLine00 }} </h1>
