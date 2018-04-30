64f256a0af1a63371eb091745f6112e0aa8408c2
jasonc1025 <jasonc@eande.world>
2018-04-30 15:39:56 -0700

    Home Vision x2

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="resumeStyle">
 
-    <h1>Vision</h1>
+    <h1>How a Vision for an <br>Open-Source, Eductional Robotics Kit... </h1>
+
+    <br>
+    <ul style="list-style:disc;">
+      <li>Since co-founding with my wife an Educational-Tech R&D Center -- <a href="http://www.eande.world/" class="urlLink" target="_blank" style="color:blue;text-decoration-line:underline;font-size:100%">Encourage and Empower (EandE)</a> -- in 2004 at my home/garage, I've enjoyed serving as Sr. Instructor and Curriculum-Engineer where we developed "T.A.C.H.nology® Robotics w/ Arduino & Raspberry Pi" as Silicon Valley's 1st Open-Source, Lower-Cost Alternative to LEGO®, TETRIX® & VEX® Robotics.</li>
+
+      <li>Innovations behind our Educational Robotics System include:
+        <ul style="list-style:disc; margin-left:2em;">
+          <li>Creative S.T.E.A.M.-Based Hardware-Chassis that integrates "T.echnology, A.rts & C.rafts... H.olistically" to inspire Industry-Grade Robotics to even Middle School students, as showcased at San Jose's Tech Museum and Bay Area's Maker Faire Premier Showroom (campus attracted 100k+ spectators),</li>
+          <li>Industry-Leading Cloud-Based Raspbery-Pi Remote-Control System (Python & C++) that helped me to receive a 1-year alliance with Siemens-eMeter in Foster City to collaborate on Web Full-Stack Technologies (JavaScript & Python) to supplement their Cloud-Based Energy Smart-Grid Systems, and</li>
+          <li>Award-Winning Google-Based 'Blockly Drag-n-Drop' C++ Coding Editor (JavaScript & Python) that assisted me in serving as Adjunct Professor for CS110 'C 'Programming at Cogswell Polytechnical College in San Jose.</li>
+        </ul>
+      </li>
+      <li>These above experiences have inspired my R&D efforts at EandE to form another Vision to Encourage and Empower our students regarding the #1 Hi-Tech Career Path… Web Development, as noted below.</li>          
+
+    </ul>
+    <br><br>
+
+
+    <h1>...Inspires a Progressive Vision for an <br>Industry-Innovative Computer Science Curriculum</h1>
     <br>
 
     <ul style="list-style:disc;">
-      <li>As <scanBold>Open-Source Technology</scanBold> matures and becomes more mainstreamed into industy, students in Computer Science  -- especially for Colleges and Secondary Schools with limited resources -- are more freed from the previous struggle of playing 'catch-up' to a once 'closed-source, limited-access & high-cost' hi-tech field of the past.</li>
+      <li>As <scanBold>Open-Source Technology</scanBold> matures and becomes more mainstreamed into industy, students in Computer Science  -- especially for Colleges and Secondary Schools with limited resources -- are more freed from the previous struggle of playing 'catch-up' to a once 'closed-source, limited-access & high-cost' Hi-Tech field of the past.</li>
       <li>And by properly harnessing this world-wide <scanBold>Open-Source Revolution</scanBold>, students can 1) be empowered to actually innovate with the 'best-of-the-best'… world-wide -- irregardless of age or socio-economic background -- and subsequently 2) be encouraged to give back to society through <scanBold>Open-Source Collaboration</scanBold>.</li>
-      <li>Thus the student learns the benefits and responsibilities of a productive community… in real life.</li>
+      <li>Thus our students do not passively-settle as 'industry-learners' -- but rather -- actively-contribute as 'industry-innovators' in responsibly building a stronger community... for life.</li>
       <br>
-      <li>Traditionally, I've enjoyed coding and teaching <scanBold>Java, C#, C++ & C</scanBold> -- among many other Linux-based languages/toolsets.</li>
-      <li>Yet due to the global pervasiveness of Web/Cloud-Services and Robotics/IOT-Devices, Stack Overflow's 2018 Global Survey of 100K Developers concludes that two Open-Source Languages -- <scanBold>JavaScript</scanBold> and <scanBold>Python</scanBold> -- have taken our world by 'storm', at least for these past several years <scanSlideRef>(Slides #1 & #2)</scanSlideRef>.</li>
-      <li>Thus from our <a href="http://www.eande.world/" class="urlLink" target="_blank" style="color:blue;text-decoration-line:underline;font-size:100%">Encourage and Empower's (EandE) Instructional R&D Lab</a>  (as based at my home/garage since 2004), I wish to present a portfolio of various commerical-grade, educational-based projects that feature <scanBold>JavaScript</scanBold> as well as <scanBold>Python</scanBold>, along with their respective core API libraries.</li>
+      <li>Traditionally, I've enjoyed coding and teaching <scanBold>Java, C#, C++ & C</scanBold> -- among many other Linux-Based languages/toolsets.</li>
+      <li>Yet due to the global pervasiveness of Web/Cloud-Services and Robotics/Electronics, Stack Overflow's 2018 Global Survey of 100K Developers concludes that two Open-Source Languages -- <scanBold>JavaScript</scanBold> and <scanBold>Python</scanBold> -- have taken our world by 'storm', at least for these past several years <scanSlideRef>(see below for Slides #1 & #2)</scanSlideRef>.</li>
+      <li>Thus from our <a href="http://www.eande.world/" class="urlLink" target="_blank" style="color:blue;text-decoration-line:underline;font-size:100%">Encourage and Empower's (EandE) Instructional R&D Lab</a>  (as based at my home/garage since 2004), I wish to present a portfolio of commerical-grade <scanBold>JavaScript</scanBold> & <scanBold>Python</scanBold> apps for a project-based Computer Science curriculum.</li>
       <br>
       
       <!-- <li style="margin-left:2em;">And within the popular JavaScript Ecosystem, the following Leading Technologies are showcased:
@@ -42,29 +61,30 @@
     <!-- Y <carousel :perPage="1" :autoplay="true" :loop="true" :navigationEnabled="true" :mouseDrag="false" paginationActiveColor="#42b983" paginationColor="#b2ebd1" paginationSize="5" easing="linear" speed="1000"> -->
     <!-- YY <carousel :perPage=1 :autoplay=true :autoplayTimeout=5000 :loop=false :navigationEnabled=true :mouseDrag=false paginationActiveColor="#42b983" paginationColor="#b2ebd1" paginationSize=20 paginationPadding=20 easing="ease" speed=1000> -->
     <carousel :perPage=1 :autoplay=true :autoplayTimeout=5000 :loop=true :navigationEnabled=true :mouseDrag=false paginationActiveColor="#42b983" paginationColor="#b2ebd1" paginationSize=20 paginationPadding=10 easing="ease" speed=1000>
+
       <slide>
         <div class="carouselHeader">
-          <h2>Slide #1:<br>Based on Stack Overflow's Worldwide 2018 Poll of 100,000 Developers,<br>Top 3 Developer-Types Share Only One Common Language: JavaScript</h2>
+          <h2><scanSlideRef>Slide #1:</scanSlideRef><br>Based on Stack Overflow's Worldwide 2018 Poll of 100,000 Developers,<br>Top 3 Developer-Types Share Only One Common Language: JavaScript</h2>
           <a href="https://insights.stackoverflow.com/survey/2018#developer-roles" class="urlLink-CarrouselHeader" target="_blank">[https://insights.stackoverflow.com/survey/2018#developer-roles]</a>
         </div>
         <!-- <img :src="/static/work/${project.category}/${project.slug}/${project.slug}_thumb.png"> -->
-        <!-- <img :src="/static/work/javascript-python-dir/11h-ArduBlockly-EandE/11h-ArduBlockly-EandE_thumb.png"> -->
+        <!-- <img :src="/static/work/robotics-dir/11h-ArduBlockly-EandE/11h-ArduBlockly-EandE_thumb.png"> -->
         <!-- <img :src="http\://image.iqing.in/recommend/613890e9-3e5d-4acd-afae-003201e1d86d.jpg-cover"> -->
-        <!-- Y src="\static\work\javascript-python-dir\11h-ArduBlockly-EandE\11h-ArduBlockly-EandE_thumb.png"> -->
+        <!-- Y src="\static\work\robotics-dir\11h-ArduBlockly-EandE\11h-ArduBlockly-EandE_thumb.png"> -->
         <!-- NOTE: ':src' not work, possibly in a non-vue '<img>' -->
-        <!-- Y <img src="/static/work/javascript-python-dir/11h-ArduBlockly-EandE/11h-ArduBlockly-EandE_thumb.png"> -->
+        <!-- Y <img src="/static/work/robotics-dir/11h-ArduBlockly-EandE/11h-ArduBlockly-EandE_thumb.png"> -->
         <img src="/static/work/home-dir/Stackoverflow-DeveloperType-Survey.png">
       </slide>
       <slide>
         <div class="carouselHeader">
-          <h2>Slide #2:<br>Based on Stack Overflow's Worldwide 2018 Poll of 100,000 Developers,<br>JavaScript, CSS, HTML (All Web-Based Tools) are Top 3 in Popularity</h2>
+          <h2><scanSlideRef>Slide #2:</scanSlideRef><br>Based on Stack Overflow's Worldwide 2018 Poll of 100,000 Developers,<br>JavaScript, CSS, HTML (All Web-Based Tools) are Top 3 in Popularity</h2>
           <a href="https://insights.stackoverflow.com/survey/2018#most-popular-technologies" class="urlLink-CarrouselHeader" target="_blank">[https://insights.stackoverflow.com/survey/2018#most-popular-technologies]</a>
         </div>
           <img src="/static/work/home-dir/Stackoverflow-MostPopularTech-JavaScript.png">
       </slide>
       <slide>
         <div class="carouselHeader">
-          <h2>Slide #3:<br>Based on GitHub's 3-Year Star-Ratings of Top 3 JS Frameworks,<br>Vue.js #1 in Greatest Momentum (React #2)</h2>
+          <h2><scanSlideRef>Slide #3:</scanSlideRef><br>Based on GitHub's 3-Year Star-Ratings of Top 3 JS Frameworks,<br>Vue.js #1 in Greatest Momentum (React #2)</h2>
           <a href="http://www.timqian.com/star-history/#facebook/react&angular/angular&vuejs/vue" class="urlLink-CarrouselHeader" target="_blank">[http://www.timqian.com/star-history/#facebook/react&angular/angular&vuejs/vue]</a>
           <br>
           <a href="https://bestof.js.org/tags/framework/trending/last-12-months" class="urlLink-CarrouselHeader" target="_blank">[https://bestof.js.org/tags/framework/trending/last-12-months]</a>
@@ -73,34 +93,30 @@
       </slide>
       <slide>
         <div class="carouselHeader">
-          <h2>Slide #4:<br>Based on Worldwide 2017 Poll of 28,000 JS Developers,<br>Vue.js #1 in Potential Market for Training (React #2)</h2>
+          <h2><scanSlideRef>Slide #4:</scanSlideRef><br>Based on Worldwide 2017 Poll of 28,000 JS Developers,<br>Vue.js #1 in Potential Market for Training (React #2)</h2>
           <a href="https://stateofjs.com/2017/front-end/results/" class="urlLink-CarrouselHeader" target="_blank">[https://stateofjs.com/2017/front-end/results/]</a>
         </div>
           <img src="/static/work/home-dir/VueVsReact-StateOfJsDotCom.png">
       </slide>
       <slide>
         <div class="carouselHeader">
-          <h2>Slide #5:<br>Based on GitHub's 2017 Star-Ratings of All JS Projects,<br>Vue.js #1 in Popularity (React #2)</h2>
+          <h2><scanSlideRef>Slide #5:</scanSlideRef><br>Based on GitHub's 2017 Star-Ratings of All JS Projects,<br>Vue.js #1 in Popularity (React #2)</h2>
           <a href="https://risingstars.js.org/2017/en/#section-all" class="urlLink-CarrouselHeader" target="_blank">[https://risingstars.js.org/2017/en/#section-all]</a>
         </div>
           <img src="/static/work/home-dir/VueVsReact-RisingStarsDotJsDotOrg.png">
       </slide>
       <slide>
         <div class="carouselHeader">
-          <h2>Slide #6:<br>Based on Stack Overflow's Worldwide 2018 Poll of 100,000 Developers,<br>Node.js #1 in Framework Popularity</h2>
+          <h2><scanSlideRef>Slide #6:</scanSlideRef><br>Based on Stack Overflow's Worldwide 2018 Poll of 100,000 Developers,<br>Node.js #1 in Framework Popularity</h2>
           <a href="https://insights.stackoverflow.com/survey/2018#technology-frameworks-libraries-and-tools" class="urlLink-CarrouselHeader" target="_blank">[https://insights.stackoverflow.com/survey/2018#technology-frameworks-libraries-and-tools]</a>
         </div>
         <img src="/static/work/home-dir/Stackoverflow-Framework-Node-Survey.png">
       </slide>
-
       
     </carousel>
-
-    <br><br>
-    <br><br>
-
-    <AccordionMenu :items_Stage01="items_Stage00"></AccordionMenu>
-
+    
+    <!-- * Archive: not need for now -->
+    <!-- Y AHG: <AccordionMenu :items_Stage01="items_Stage00"></AccordionMenu> -->
 
   </div>
 </template>
@@ -213,6 +229,9 @@ $backgroundColor_Black: #000000 ;  // rgba(0, 0, 0, 1)
  scanBold {
    font-weight:500;
  }
+  scanBold {
+   font-weight:500;
+ } 
  scanSlideRef {
    color: red;
    font-weight:500;
