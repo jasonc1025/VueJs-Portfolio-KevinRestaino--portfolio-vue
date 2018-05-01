4dc180d06a6d766155292b336e55f68b2cf1b717
jasonc1025 <jasonc@eande.world>
2018-05-01 03:02:50 -0700

    Centered Menu

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -7,19 +7,20 @@
 
     <router-link v-if="store.getters.tabModeGet==1" tag="a" to="/projects-react" @click.native="store.commit('tabModeSet','1')" ><b>React</b></router-link>
     <!-- YN causes desktop to be 32px but mobile 16px (not 32px): <router-link v-else tag="a" to="/projects-react" @click.native="store.commit('tabModeSet','1')" style="font-size:32px;">2React</router-link> -->
-    <router-link v-else tag="a" to="/projects-react" @click.native="store.commit('tabModeSet','1')">React</router-link>
+    <router-link v-else tag="a" to="/projects-react" @click.native="store.commit('tabModeSet','1')">Web FrontEnd:<br>JavaScript React</router-link>
     
     <router-link v-if="store.getters.tabModeGet==2" tag="a" to="/projects-vue-js" @click.native="store.commit('tabModeSet','2')"><b>Vue.js</b></router-link>
-    <router-link v-else tag="a" to="/projects-vue-js" @click.native="store.commit('tabModeSet','2')">Vue.js</router-link>
+    <router-link v-else tag="a" to="/projects-vue-js" @click.native="store.commit('tabModeSet','2')">Web FrontEnd:<br>JavaScript Vue.js</router-link>
 
     <router-link v-if="store.getters.tabModeGet==3" tag="a" to="/projects-node-js" @click.native="store.commit('tabModeSet','3')"><b>Node.js</b></router-link>
-    <router-link v-else tag="a" to="/projects-node-js" @click.native="store.commit('tabModeSet','3')">Node.js</router-link>
+    <router-link v-else tag="a" to="/projects-node-js" @click.native="store.commit('tabModeSet','3')">Web BackEnd:<br>JavaScript Node.js</router-link>
 
     <router-link v-if="store.getters.tabModeGet==4" tag="a" to="/projects-robotics" @click.native="store.commit('tabModeSet','4')"><b>Robotics</b></router-link>
-    <router-link v-else tag="a" to="/projects-robotics" @click.native="store.commit('tabModeSet','4')">Robotics</router-link>
+    <!-- <router-link v-else tag="a" to="/projects-robotics" @click.native="store.commit('tabModeSet','4')" style="text-align:center;"><scan style="text-align:center;">JavaScript/Python:<br>Robotics</scan></router-link> -->
+    <router-link v-else tag="a" to="/projects-robotics" @click.native="store.commit('tabModeSet','4')">JavaScript/Python:<br>Robotics</router-link>
 
     <router-link v-if="store.getters.tabModeGet==5" tag="a" to="/projects-industry-siemens" @click.native="store.commit('tabModeSet','5')"><b>Industry:Siemens</b></router-link>
-    <router-link v-else tag="a" to="/projects-industry-siemens" @click.native="store.commit('tabModeSet','5')">Industry:Siemens</router-link>
+    <router-link v-else tag="a" to="/projects-industry-siemens" @click.native="store.commit('tabModeSet','5')">JavaScript/Python:<br>Industry:Siemens</router-link>
 
     <router-link v-if="store.getters.tabModeGet==6" tag="a" to="/resume" @click.native="store.commit('tabModeSet','6')"><b>Resum&eacute;</b></router-link>
     <router-link v-else tag="a" to="/resume" @click.native="store.commit('tabModeSet','6')">Resum&eacute;</router-link>
@@ -67,6 +68,7 @@ export default {
   a {
     border-bottom: 1px solid transparent;
     transition: 0.3s;
+    text-align:center;
 
     &:hover {
       border-bottom: 1px solid;
