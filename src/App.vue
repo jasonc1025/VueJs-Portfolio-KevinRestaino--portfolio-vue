6a1c78a5c37fa4d1b157d0c45e452c6077858c4d
jasonc1025 <jasonc@eande.world>
2018-05-01 03:02:35 -0700

    Missing ';'

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
     </section>
     <footer class="footer">
       <!-- <span> Portfolio developed using Vue.js & Netlify <br> © {{ currentYear }} Encourage and Empower. All rights reserved.</span> -->
-      <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /> &nbsp </a><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Portfolio developed by Jason W Chan using Vue.js, Node.js & Netlify <br> Creative Commons Attribution-NonCommercial-ShareAlike <br> 4.0 International License</a>
+      <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /> &nbsp; </a><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Portfolio developed by Jason W Chan using Vue.js, Node.js & Netlify <br> Creative Commons Attribution-NonCommercial-ShareAlike <br> 4.0 International License</a>
     </footer>
   </div>
 </template>
